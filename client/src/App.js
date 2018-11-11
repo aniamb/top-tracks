647d8c8ed2bd96176557cb2c80c0ac841a0be1af
Anita <aambalav@purdue.edu>
2018-11-10 22:55:37 -0600

    basic functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,16 @@ class App extends Component {
 	}
     this.state = {
 	loggedIn: token ? true : false,
-	topTracks: {name: 'not checked'}
+	topTracks: {name:''},
+	topTracks2: {name:''},
+	topTracks3: {name:''},
+	topTracks4: {name:''},
+	topTracks5: {name:''},
+	topTracks6: {name:''},
+	topTracks7: {name:''},
+	topTracks8: {name:''},
+	topTracks9: {name:''},
+	topTracks10: {name:''}
     }
   }
 
@@ -36,7 +45,34 @@ class App extends Component {
  .then((response) => {
    this.setState({
      topTracks: {
-       name: response.item.name
+       name: response.items[0].name
+     },
+     topTracks2: {
+	name: response.items[1].name
+     },
+     topTracks3: {
+	name: response.items[2].name
+     },
+     topTracks4: {
+	name: response.items[3].name
+     },
+     topTracks5: {
+	name: response.items[4].name
+     },
+     topTracks6: {
+	name: response.items[5].name
+     },
+     topTracks7: {
+	name: response.items[6].name
+     },
+     topTracks8: {
+	name: response.items[7].name
+     },
+     topTracks9: {
+	name: response.items[8].name
+     },
+     topTracks10: {
+	name: response.items[9].name
      }
 
    });
@@ -44,10 +80,29 @@ class App extends Component {
 }
   render() {
     return (
-      <div className="App">
+	 <div className="App">
      	<a href='http://localhost:8888'> Login to Spotify </a>
 	<div>
-		Top Tracks: {this.state.topTracks.name}
+		#1: {this.state.topTracks.name}
+			<br></br>
+		#2: {this.state.topTracks2.name}
+			<br></br>
+		#3: {this.state.topTracks3.name}
+			<br></br>
+		#4: {this.state.topTracks4.name}
+			<br></br>
+		#5: {this.state.topTracks5.name}
+			<br></br>
+		#6: {this.state.topTracks6.name}
+			<br></br>
+		#7: {this.state.topTracks7.name}
+			<br></br>
+		#8: {this.state.topTracks8.name}
+			<br></br>
+		#9: {this.state.topTracks9.name}
+			<br></br>
+		#10: {this.state.topTracks10.name}
+
 	</div>
 	{ this.state.loggedIn &&
 		<button onClick={() => this.getTopTracks()}>
